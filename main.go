b1b6519a2b128247da684fb515cec8242bd2c884
yhayyani <youssef16hay@gmail.com>
2024-11-18 17:59:31 +0100

    netcat

--- main.go
+++ main.go
@@ -2,110 +2,25 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net"
+	serverNet "netcat/src"
 	"os"
-	"strings"
-	"sync"
-	"time"
+	"strconv"
 )
 
-type server struct {
-	listeaAdder string
-	Ln          net.Listener
-	Clients     map[net.Conn]string
-	History     string
-}
-
-func newServer(name string) *server {
-	return &server{listeaAdder: name}
-}
-
-func (s *server) start() {
-	ln, err := net.Listen("tcp", s.listeaAdder)
-	if err != nil {
-		log.Fatal(err)
-	}
-	s.Ln = ln
-	defer ln.Close()
-	s.Clients = make(map[net.Conn]string)
-	s.acceptConnections()
-
-}
-
-func (s *server) acceptConnections() {
-	for {
-		conn, err := s.Ln.Accept()
-		if err != nil {
-			log.Fatal(err)
+func main() {
+	port := ":8989" // default port
+	if len(os.Args) == 2 {
+		if _, err := strconv.Atoi(os.Args[1]); err == nil {
+			port = ":" + os.Args[1]
+		} else {
+			fmt.Println("[USAGE]: ./TCPChat $port")
+			return
 		}
-		go s.readconnection(conn)
-	}
-}
-func (s *server) close(conn net.Conn) {
-	conn.Close()
-	for client, _ := range s.Clients {
-		client.Write([]byte(fmt.Sprintf("%s has left our chat...", strings.TrimSpace(s.Clients[conn]))))
-	}
-	delete(s.Clients, conn)
-}
-
-func messages(conn net.Conn, c chan []byte) {
-	buf := make([]byte, 2048)
-	n, err := conn.Read(buf)
-	if err != nil {
-		fmt.Println("Error reading:", err.Error())
-		conn.Close()
+	} else if len(os.Args) > 2 {
+		fmt.Println("[USAGE]: ./TCPChat $port")
 		return
 	}
-	msg := buf[:n]
-	c <- msg
-}
 
-func (s *server) readconnection(conn net.Conn) {
-	var mu sync.Mutex
-	defer s.close(conn)
-	conn.Write([]byte("Welcome to TCP-Chat!\n         _nnnn_\n        dGGGGMMb\n       @p~qp~~qMb\n       M|@||@) M|\n       @,----.JM|\n      JS^\\__/  qKL\n     dZP        qKRb\n    dZP          qKKb\n   fZP            SMMb\n   HZM            MMMM\n   FqM            MMMM\n __| \".        |\\dS\"qML\n |    `.       | `' \\Zq\n_)      \\.___.,|     .'\n\\____   )MMMMMP|   .'\n     `-'       `--'\n[ENTER YOUR NAME]: "))
-	buf := make([]byte, 2048)
-	n, _ := conn.Read(buf)
-	mu.Lock()
-	s.Clients[conn] = strings.TrimSpace(string(buf[:n]))
-	mu.Unlock()
-	conn.Write([]byte(s.History))
-	s.History += fmt.Sprintf("%s has joined the chat...\n", s.Clients[conn])
-	for client := range s.Clients {
-		if client != conn {
-			client.Write([]byte(fmt.Sprintf("%s has joined our chat...\n", s.Clients[conn])))
-		}
-	}
-	c := make(chan []byte)
-
-	for {
-		go func() {
-			messages(conn, c)
-		}()
-		msg := <-c
-		if msg == nil {
-			break
-		}
-
-		ss := strings.TrimSpace(s.Clients[conn])
-		formattedMessage := fmt.Sprintf("[%s][%s]: %s", time.Now().Format("2006-01-02 15:04:05"), ss, msg)
-		s.History += formattedMessage
-
-		for client := range s.Clients {
-			if client != conn {
-				client.Write([]byte(formattedMessage))
-			}
-		}
-	}
-}
-
-func main() {
-	port := "8080"
-	if len(os.Args) == 2 {
-		port = os.Args[1]
-	}
-	s := newServer(":" + port)
-	s.start()
+	server := serverNet.NewServer(port)
+	server.Start()
 }
---
+++ src/server.go
@@ -0,0 +1,157 @@
+package serverNet
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"sync"
+	"time"
+)
+
+const MaxConnections = 10
+
+type server struct {
+	Listenaddress string
+	Ln            net.Listener
+	Clients       map[net.Conn]string
+	History       string
+	Mu            sync.Mutex
+}
+
+// NewServer initializes a new server
+func NewServer(port string) *server {
+	return &server{
+		Listenaddress: port,
+		Clients:       make(map[net.Conn]string),
+	}
+}
+
+// Start the server and listen for connections
+func (s *server) Start() {
+	ln, err := net.Listen("tcp", s.Listenaddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ln.Close()
+
+	s.Ln = ln
+	fmt.Println("Server started on", s.Listenaddress)
+	s.AcceptConnections()
+}
+
+// Accepts new client connections
+func (s *server) AcceptConnections() {
+	for {
+		//connection between server and client
+		con, err := s.Ln.Accept()
+		if err != nil {
+			log.Println("Error accepting connection:", err)
+			continue
+		}
+
+		// Check if we have reached the maximum number of connections
+		if len(s.Clients) >= MaxConnections {
+			fmt.Fprintln(con, "Server is full. Connection rejected.")
+			con.Close()
+			fmt.Println("Rejected connection from", con.RemoteAddr())
+			continue
+		}
+
+		// Handle the connection in a new goroutine
+		go s.HandleConnection(con)
+	}
+}
+
+// Manages a single client connection
+func (s *server) HandleConnection(con net.Conn) {
+	defer s.CloseConnection(con)
+
+	con.Write([]byte("Welcome to TCP-Chat!\n         _nnnn_\n        dGGGGMMb\n       @p~qp~~qMb\n       M|@||@) M|\n       @,----.JM|\n      JS^\\__/  qKL\n     dZP        qKRb\n    dZP          qKKb\n   fZP            SMMb\n   HZM            MMMM\n   FqM            MMMM\n __| \".        |\\dS\"qML\n |    `.       | `' \\Zq\n_)      \\.___.,|     .'\n\\____   )MMMMMP|   .'\n     `-'       `--'\n[ENTER YOUR NAME]: "))
+
+	// Reading client's name
+	buf := make([]byte, 1024)
+	n, err := con.Read(buf)
+	if err != nil {
+		log.Println("Error reading name:", err)
+		return
+	}
+
+	clientName := strings.TrimSpace(string(buf[:n]))
+
+	s.Mu.Lock()
+	s.Clients[con] = clientName
+	s.Mu.Unlock()
+
+	// Send chat history to the new client
+	con.Write([]byte(s.History))
+
+	// Notify other clients
+	s.BroadcastMessage(fmt.Sprintf("%s has joined our chat...", clientName), con)
+
+	//  receive messages
+	msgChan := make(chan []byte)
+
+	//  read messages from the client
+	go func() {
+		defer close(msgChan)
+		buf := make([]byte, 2048)
+		for {
+			con.Write([]byte(fmt.Sprintf("[%s][%s]:", time.Now().Format("2006-01-02 15:04:05"), clientName)))
+			n, err := con.Read(buf)
+			if err != nil {
+				if err.Error() == "EOF" {
+					log.Println("Client closed connection gracefully.")
+				} else {
+					log.Println("Client read error:", err)
+				}
+				return
+			}
+			if n > 0 {
+				msgChan <- buf[:n]
+			}
+		}
+	}()
+
+	// Listen for messages from the client
+	for msg := range msgChan {
+		if msg == nil || strings.TrimSpace(string(msg)) == "" {
+			continue
+		}
+		formattedMessage := fmt.Sprintf("[%s][%s]: %s", time.Now().Format("2006-01-02 15:04:05"), clientName, strings.TrimSpace(string(msg)))
+
+		s.Mu.Lock()
+		s.History += formattedMessage + "\n"
+		s.Mu.Unlock()
+
+		s.BroadcastMessage(formattedMessage, con)
+	}
+
+}
+
+//  handles disconnection of a client
+func (s *server) CloseConnection(con net.Conn) {
+	s.Mu.Lock()
+	clientName := s.Clients[con]
+	delete(s.Clients, con)
+	s.Mu.Unlock()
+
+	// Notify other clients
+	exitMessage := fmt.Sprintf("%s has left our chat...", clientName)
+	s.BroadcastMessage(exitMessage, nil)
+	con.Close()
+}
+
+func (s *server) BroadcastMessage(message string, sender net.Conn) {
+	s.Mu.Lock()
+	defer s.Mu.Unlock()
+
+	for client, clientName := range s.Clients {
+		if sender != client {
+			_, err := client.Write([]byte("\n" + message + "\n" + fmt.Sprintf("[%s][%s]:", time.Now().Format("2006-01-02 15:04:05"), clientName)))
+			if err != nil {
+				log.Println("Error sending message:", err)
+			}
+		}
+	}
+}
